4dc4a3b28c949b945759e6703d4b47e04f25f3ff
Jackson <jacksonludwig0@gmail.com>
2020-02-08 19:54:35 -0500

    start

---
+++ intro1/target/debug/deps/intro1.d
@@ -0,0 +1,5 @@
+D:\Rust-General\intro1\target\debug\deps\intro1.exe: src\main.rs
+
+D:\Rust-General\intro1\target\debug\deps\intro1.d: src\main.rs
+
+src\main.rs:
---
+++ intro1/target/debug/intro1.d
@@ -0,0 +1,1 @@
+D:\Rust-General\intro1\target\debug\intro1.exe: D:\Rust-General\intro1\src\main.rs
