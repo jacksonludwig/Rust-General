220c60135193284a87765524e307346f33558683
Jackson <jacksonludwig0@gmail.com>
2020-02-17 00:03:54 -0500

    error handling

---
+++ what_type_of_error/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    loop {
+        // --snip--
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100.");
+            continue;
+        }
+
+        match guess.cmp(&secret_number) {
+            // --snip--
+        }
+    }
+}
+
+// below allows you to avoid extra checks (above is bad)
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
